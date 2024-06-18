3d8430dc3f975ff9da38a32e4f8964b03bca9cdf
ddurmala <169076397+ddurmala@users.noreply.github.com>
2024-06-18 12:42:20 -0400

    finished variables, data typ & array basics

--- js/script.js
+++ js/script.js
@@ -1,7 +1,44 @@
-var firstName = 'Dana';
+var firstName = 'Dana'; //string
+var lastName = 'Castagna';
+var age = 39; //Number
+var isADev = true; //Boolean
+var un = undefined; //Undefined - provided by JS
+var n = null; //programmer's representation of nothing
+var notANumber = NaN; //Not a Number  - this is provided when u try to process a mathematical equation and the result is not a valid # // provided by JS
 
-console.log(typeof 3);
+var nothing;
 
-console.log(5 + 5);
+//age = age + 1;
 
-console.log(firstName);
+//console.log(age);
+
+// firstName + 'Castagna';
+
+var fullName = firstName + ' ' + lastName;
+
+// console.log(fullName);
+
+var names = ['dana', 'nick', 'julie', 'justine', 'kevin', 'lynn'];
+var ages = [30, 19, 22, 45]
+
+
+names.push('susan');
+
+names.unshift('pete');
+
+names.pop();
+
+
+console.log(names);
+
+
+//console.log(names[names.length - 1]);
+
+//names[1] = 'joe';
+
+//console.log(names);
+
+//ages[2] += 10;
+
+//console.log(ages);
+
