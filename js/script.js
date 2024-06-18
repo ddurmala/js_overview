639b5eb0f7671c82c1a1c9b7514d49240f6f295b
ddurmala <169076397+ddurmala@users.noreply.github.com>
2024-06-18 13:31:15 -0400

    finished basic operators

--- js/script.js
+++ js/script.js
@@ -1,44 +1,9 @@
 var firstName = 'Dana'; //string
 var lastName = 'Castagna';
 var age = 39; //Number
-var isADev = true; //Boolean
-var un = undefined; //Undefined - provided by JS
-var n = null; //programmer's representation of nothing
-var notANumber = NaN; //Not a Number  - this is provided when u try to process a mathematical equation and the result is not a valid # // provided by JS
+var names = ['dana', 'nick', 'julie', 'justine', 'kevin', 'lynn', 'pete', 'lauren', 'maddie'];
 
-var nothing;
+//comparison operators
+console.log(age === '39');
 
-//age = age + 1;
-
-//console.log(age);
-
-// firstName + 'Castagna';
-
-var fullName = firstName + ' ' + lastName;
-
-// console.log(fullName);
-
-var names = ['dana', 'nick', 'julie', 'justine', 'kevin', 'lynn'];
-var ages = [30, 19, 22, 45]
-
-
-names.push('susan');
-
-names.unshift('pete');
-
-names.pop();
-
-
-console.log(names);
-
-
-//console.log(names[names.length - 1]);
-
-//names[1] = 'joe';
-
-//console.log(names);
-
-//ages[2] += 10;
-
-//console.log(ages);
-
+console.log(!50);
