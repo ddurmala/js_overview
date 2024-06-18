47e7eb1d0be881b458add1bc3997ff4fa59eaa17
ddurmala <169076397+ddurmala@users.noreply.github.com>
2024-06-18 14:49:58 -0400

    end of day

--- js/script.js
+++ js/script.js
@@ -2,8 +2,24 @@ var firstName = 'Dana'; //string
 var lastName = 'Castagna';
 var age = 39; //Number
 var names = ['dana', 'nick', 'julie', 'justine', 'kevin', 'lynn', 'pete', 'lauren', 'maddie'];
+var oldEnough = age >= 21;
 
-//comparison operators
-console.log(age === '39');
+if (firstName && lastName) {
+    console.log('full name received');
+}
+else if (!firstName) {
+    console.log('you must provide your first name')
+}
+else if (!lastName) {
+    console.log('you must provide your last name')
+}
+else {
+    console.log('you must provide your first name')
+}
 
-console.log(!50);
+if (oldEnough) {
+    console.log('you may enter!');
+}
+else {
+    console.log('you must be over 21 to enter');
+}
