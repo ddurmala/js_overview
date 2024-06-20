34f31cd52afc842af325132d16da088a8e81d0c6
ddurmala <169076397+ddurmala@users.noreply.github.com>
2024-06-20 13:05:39 -0400

    finished basics of loops

--- js/script.js
+++ js/script.js
@@ -1,25 +1,19 @@
-var firstName = 'Dana'; //string
-var lastName = 'Castagna';
-var age = 39; //Number
 var names = ['dana', 'nick', 'julie', 'justine', 'kevin', 'lynn', 'pete', 'lauren', 'maddie'];
-var oldEnough = age >= 21;
+var users = ['Bob', 99, 'Ann', 35, 'Billy', 45];
 
-if (firstName && lastName) {
-    console.log('full name received');
-}
-else if (!firstName) {
-    console.log('you must provide your first name')
-}
-else if (!lastName) {
-    console.log('you must provide your last name')
-}
-else {
-    console.log('you must provide your first name')
-}
+//Print count from 1 to 10
+//for (var count = 1; count <= 10; count++) {
+//    console.log(count)
+//}
+//
+/*for (var index = 0; index < names.length; index++) {
+    console.log(names[index]);
+}*/
 
-if (oldEnough) {
-    console.log('you may enter!');
-}
-else {
-    console.log('you must be over 21 to enter');
+var count = 1;
+
+while (count <= 10) {
+    console.log(count);
+
+    count++;
 }
