36cc4c12bc118f4bc81a5602c84fb57e7a233dce
ddurmala <169076397+ddurmala@users.noreply.github.com>
2024-06-21 10:20:26 -0400

    functions

---
+++ js/script.js
@@ -0,0 +1,29 @@
+
+/*function myFirstFunc() {
+    console.log('function has been called')
+}
+
+myFirstFunc();*/
+
+/*function add(num1, num2) {
+    console.log(num1 + num2)
+}
+
+add(5, 10);
+
+add(37, 78);
+*/
+
+/*function printValues(num, name, boolean, another) {
+    console.log(boolean);
+}
+
+printValues(10, 'Allen', false, 'another');*/
+
+function printNames(names) {
+    for (const name of names) {
+        console.log(name)
+    }
+}
+
+printNames(['Bob', 'Jim', 'Julie']);
